c84db931cba1be89145dc28496c3a47b43a76d3d
Artsiom Rak <ra44elo@gmail.com>
2020-10-10 20:48:18 +0300

    fix: fix deletion of a user, delete logs

--- src/resources/tasks/task.memory.repository.js
+++ src/resources/tasks/task.memory.repository.js
@@ -10,37 +10,36 @@ const getByBoardIdAndTaskId = async (boardId, taskId) => {
 
 const createTask = async task => {
   db.tasks.push(task);
-  console.log(task);
   return task;
 };
 
 const updateTask = async (boardId, taskId, task) => {
   db.tasks = db.tasks.map(t => {
     if (t.boardId === boardId && t.id === taskId) {
-      // const output = {};
+      const output = {};
 
-      // if (task.title) {
-      //   output.title = task.title;
-      // }
-      // if (task.order) {
-      //   output.order = task.order;
-      // }
-      // if (task.description) {
-      //   output.description = task.description;
-      // }
-      // if (task.userId || task.userId === null) {
-      //   output.userId = task.userId;
-      // }
-      // if (task.boardId || task.boardId === null) {
-      //   output.boardId = task.boardId;
-      // }
-      // if (task.columnId || task.columnId === null) {
-      //   output.columnId = task.columnId;
-      // }
+      if (task.title) {
+        output.title = task.title;
+      }
+      if (task.order) {
+        output.order = task.order;
+      }
+      if (task.description) {
+        output.description = task.description;
+      }
+      if (task.userId || task.userId === null) {
+        output.userId = task.userId;
+      }
+      if (task.boardId || task.boardId === null) {
+        output.boardId = task.boardId;
+      }
+      if (task.columnId || task.columnId === null) {
+        output.columnId = task.columnId;
+      }
 
       return {
         ...t,
-        ...task
+        ...output
       };
     }
 
